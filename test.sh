3152e2d5cf7c47b836012bba95fd7ed80cf45928
fstdawn <74463077+fstdawn@users.noreply.github.com>
2021-01-22 16:07:56 +1100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##########################################################################
+# Install tflint: https://github.com/terraform-linters/tflint#installation
+##########################################################################
+
+TFLINT_VERSION=0.23.1
+TF_DIRS=("module/nsxt/" "module/sddc/" "${ENV_NAME}/")
+curl https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+
+for tf_directory in "${TF_DIRS[@]}"
+do
+    echo "linting ${tf_directory}"
+    tflint $tf_directory
+done
