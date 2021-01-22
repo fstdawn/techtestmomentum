cdee909a9ff5aec3155448a7043dcad1baa1aa5f
fstdawn <74463077+fstdawn@users.noreply.github.com>
2021-01-22 21:06:11 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,9 @@
 # Install tflint: https://github.com/terraform-linters/tflint#installation
 ##########################################################################
 
-TFLINT_VERSION=0.23.1
 pwd
 ls -al
 TF_DIRS=("techtestmomentum/vpc/")
-curl https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
 
 for tf_directory in "${TF_DIRS[@]}"
 do
